df5c3b6c406e2fe7e02b392db12f07ba33d7dca3
chalupaul <me@chalupaul.com>
2021-03-14 12:15:05 -0500

    totally had 2 files in the same place

--- python_docker/gate_tests.sh
+++ python_docker/gate_tests.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
-
+set +x
 # This script assumes its run in a directory with ./app and ./tests local to it
 
 DEFAULT_DIR="/opt/pysetup"
 LOCAL_DIR=${1:-$DEFAULT_DIR}
 
-poetry check
-flake8 \
-    --max-line-length=88 \
-    --count \
-    --statistics \
-    ./app ./tests
-black \
-    --config ${LOCAL_DIR}/pyproject.toml \
-    --check \
-    ./app ./tests
-isort \
-    --settings-path ${LOCAL_DIR}/pyproject.toml --check-only ./app ./tests
-bandit -r ./app ./tests
-safety check --cache --full-report
-coverage run -m --branch --omit='*/tests/*' --source ./app pytest -vv ./tests && coverage report -m --skip-covered --skip-empty --fail-under=75 
+
+commands=( \
+    "cd ${LOCAL_DIR} && \
+        poetry check && \
+        cd -" \
+    "flake8 \
+        --max-line-length=88 \
+        --count \
+        --statistics \
+        ./app ./tests" \
+    "black \
+        --config ${LOCAL_DIR}/pyproject.toml \
+        --check \
+        ./app ./tests" \
+    "isort \
+        --settings-path ${LOCAL_DIR}/pyproject.toml \
+        --check-only \
+        ./app ./tests" \
+    "bandit \
+        -r ./app ./tests \
+    safety check \
+        --cache \
+        --full-report" \
+    "coverage run \
+        -m \
+        --branch \
+        --omit='*/tests/*' \
+        --source ./app \
+            pytest -vv \
+                ./tests && \
+                coverage report \
+                    -m \
+                    --skip-covered \
+                    --skip-empty \
+                    --fail-under=75 " \
+)
+
+for c in ${commands[@]}; do
+    eval $c
+    [[ $? != 0 ]] && exit 1
+done
